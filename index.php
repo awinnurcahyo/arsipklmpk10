ceb851548590483402345a5bb23e28e77f6616ab
awinnurcahyo <153823326+awinnurcahyo@users.noreply.github.com>
2024-06-23 14:40:17 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
     <!doctype html>
     <html class="no-js" lang="en">
 
-    <head></head>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Dashboard V.2 | Kiaalap - Kiaalap Admin Template</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    </head>
 
     <body>
 
